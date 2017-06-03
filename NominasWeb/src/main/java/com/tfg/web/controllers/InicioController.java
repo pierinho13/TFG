582c54193285d32ddd9d1eb4baf7ef9cb58eef9c
Piero Rospigliosi <pierinho13@gmail.com>
2017-06-03 16:26:18 +0200

    nuevo nominas web proyecto MVC desde cero funcionando  :D

---
+++ NominasWeb/src/main/java/com/tfg/web/controllers/InicioController.java
@@ -0,0 +1,13 @@
+package com.tfg.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class InicioController {
+	@RequestMapping(value = {"/","/inicio","/login"}, method = RequestMethod.GET)
+	public String inicioLogin(){
+		return "login";
+	}
+}
